47b544c2a6e2b98c9ef52c965ff905b9dc6ecde2
Le Ax <axelsoot@gmail.com>
2022-12-04 15:39:32 -0800

    Second star of day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,5 +1,5 @@
 def main():
-    most_calories = 0
+    most_calories = [0, 0, 0]
     current_tally = 0
     file = open('input', 'r')
     lines = file.readlines()
@@ -7,12 +7,14 @@ def main():
         line = line.rstrip()
         if line == '':
             print('new elf, prev tally was ' + str(current_tally))
-            if most_calories < current_tally:
-                most_calories = current_tally
+            most_calories.append(current_tally)
+            most_calories.sort(reverse=True)
+            most_calories = most_calories[0:-1]
             current_tally = 0
         else:
             current_tally += int(line)
     print('Largest calories count: ' + str(most_calories))
+    print('3 largest = {}+{}+{}={}'.format(most_calories[0], most_calories[1], most_calories[2], sum(most_calories)))
 
 if __name__ == '__main__':
     main()
