4db99497005a3421bc93ff98ef97291378535be0
Le Ax <axelsoot@gmail.com>
2022-12-04 16:00:44 -0800

    day 2 first star

---
+++ day2/day2.py
@@ -0,0 +1,63 @@
+from enum import Enum
+
+
+class Janken(Enum):
+    Rock = 1
+    Paper = 2
+    Scissors = 3
+
+
+def battle_score(opponent, me):
+    """ return score based on who plays what """
+    if opponent == me:
+        return 3
+    if opponent == Janken.Rock:
+        if me == Janken.Scissors:
+            return 0
+        return 6  # me == Paper
+    if opponent == Janken.Paper:
+        if me == Janken.Rock:
+            return 0
+        return 6  # me == Scissors
+    if me == Janken.Paper:  # opponent == Scissors
+        return 0
+    return 6  # me == Rock
+
+
+def opponent2play(opponent):
+    if opponent == "A":
+        return Janken.Rock
+    if opponent == "B":
+        return Janken.Paper
+    if opponent == "C":
+        return Janken.Scissors
+    raise Exception(opponent)
+
+
+def me2play(me):
+    if me == "X":
+        return Janken.Rock
+    if me == "Y":
+        return Janken.Paper
+    if me == "Z":
+        return Janken.Scissors
+    raise Exception(me)
+
+
+def main():
+    file = open('input', 'r')
+    lines = file.readlines()
+    score = 0
+    for line in lines:
+        line = line.rstrip()
+        if line == '':
+            break
+        [opponent, me] = line.split(" ")
+        opponent = opponent2play(opponent)
+        me = me2play(me)
+        score += battle_score(opponent, me) + me.value
+    print('my score would be: ' + str(score))
+
+
+if __name__ == '__main__':
+    main()
