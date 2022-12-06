9cdc9256d84a2c1cb795d139df370f6389d3bdbc
Le Ax <axelsoot@gmail.com>
2022-12-05 21:17:04 -0800

    day 6 star 1

---
+++ day6/day6.py
@@ -0,0 +1,22 @@
+
+def main():
+    four = []
+    count = 0
+    file = open('input', 'r')
+    characters = file.read()
+    for character in characters:
+        four.append(character)
+        count += 1
+        all_different = True
+        for i in range(len(four)-1):
+            if character == four[i]:
+                all_different = False
+                four = four[i+1:]
+                break
+        if all_different and len(four) == 4:
+            print("Got signal at count {}".format(count))
+            return
+
+
+if __name__ == '__main__':
+    main()
