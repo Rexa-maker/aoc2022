91391edaf9c49545c9ae5ccd52917a9ce44b22dc
Le Ax <axelsoot@gmail.com>
2022-12-06 22:22:06 -0800

    day 7 star 2

--- day7/day7.py
+++ day7/day7.py
@@ -1,6 +1,8 @@
 import re
 
-SMOLL_MAX_SIZE = 100000
+FILE_SYSTEM_SIZE = 70000000
+UPDATE_SIZE      = 30000000
+SMOLL_MAX_SIZE   = 100000
 CD_COMMAND_REGEXP   = re.compile(r"^\$ cd (.*)$")
 LS_COMMAND_REGEXP   = re.compile(r"^\$ ls$")
 DIR_LISTING_REGEXP  = re.compile(r"^dir (.*)$")
@@ -19,8 +21,6 @@ def discover_file_system(lines):
 
         if len(line) == 0:
             continue
-
-        print(line)
 
         m = CD_COMMAND_REGEXP.match(line)
         if m:
@@ -76,12 +76,12 @@ def discover_file_system(lines):
     return file_system
 
 
-def get_size_of_dir_and_smoll_tally(dir):
+def get_size_of_dir_n_smoll_tally(dir):
     size = sum(dir['files'].values())
     smoll_tally = 0
 
     for sub_dir in dir['dirs']:
-        [sub_dir_size, sub_dir_smoll_tally] = get_size_of_dir_and_smoll_tally(dir['dirs'][sub_dir])
+        [sub_dir_size, sub_dir_smoll_tally] = get_size_of_dir_n_smoll_tally(dir['dirs'][sub_dir])
         size += sub_dir_size
         smoll_tally += sub_dir_smoll_tally
 
@@ -91,15 +91,37 @@ def get_size_of_dir_and_smoll_tally(dir):
     return [size, smoll_tally]
 
 
+def get_smollest4update(dir, needed_freed_size):
+    size = sum(dir['files'].values())
+    sub_dirs_smollest_smollest4update = None
+
+    for sub_dir in dir['dirs']:
+        [sub_dir_size, smollest4update] = get_smollest4update(dir['dirs'][sub_dir], needed_freed_size)
+        size += sub_dir_size
+        if smollest4update and (not sub_dirs_smollest_smollest4update or smollest4update < sub_dirs_smollest_smollest4update):
+            sub_dirs_smollest_smollest4update = smollest4update
+
+    if size >= needed_freed_size and (not sub_dirs_smollest_smollest4update or size < sub_dirs_smollest_smollest4update):
+        sub_dirs_smollest_smollest4update = size
+
+    return [size, sub_dirs_smollest_smollest4update]
+
+
 def main():
     file = open('input', 'r')
     lines = file.readlines()
 
     file_system = discover_file_system(lines)
 
-    [_, sum_of_smolls] = get_size_of_dir_and_smoll_tally(file_system)
+    [sizeof_root, sum_of_smolls] = get_size_of_dir_n_smoll_tally(file_system)
 
+    free_size = FILE_SYSTEM_SIZE - sizeof_root
+    needed_freed_size = UPDATE_SIZE - free_size
+    [_, smollest4update] = get_smollest4update(file_system, needed_freed_size)
+
+    print('size of /: {}'.format(str(sizeof_root)))
     print('sum of all dirs of size <= {}: {}'.format(str(SMOLL_MAX_SIZE), str(sum_of_smolls)))
+    print('smollest dir size for update {}'.format(smollest4update))
 
 
 if __name__ == '__main__':
