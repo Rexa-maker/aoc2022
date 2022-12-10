6ad121f0b8ab68855b5106cc6caa0a2f86c07705
Le Ax <axelsoot@gmail.com>
2022-12-10 11:52:50 -0800

    day 9 star 2

--- day9/day9.py
+++ day9/day9.py
@@ -4,35 +4,38 @@ def main():
     file = open('input', 'r')
     lines = file.readlines()
 
-    head = np.array([0, 0])
-    tail = np.array([0, 0])
-    visited = set([(0, 0)])
+    for num_knots in (2, 10):
+        knots = np.zeros([num_knots, 2], int)
+        visited = set([(0, 0)])
 
-    for line in lines:
-        line = line.rstrip()
-        if len(line) == 0:
-            continue
+        for line in lines:
+            line = line.rstrip()
+            if len(line) == 0:
+                continue
 
-        [direction, count] = line.split(" ")
-        count = int(count)
+            [direction, count] = line.split(" ")
+            count = int(count)
 
-        if direction == 'R':
-            direction = [1, 0]
-        elif direction == 'L':
-            direction = [-1, 0]
-        elif direction == 'U':
-            direction = [0, 1]
-        else:
-            direction = [0, -1]
+            if direction == 'R':
+                direction = [1, 0]
+            elif direction == 'L':
+                direction = [-1, 0]
+            elif direction == 'U':
+                direction = [0, 1]
+            else:
+                direction = [0, -1]
 
-        for i in range(count):
-            head += direction
-            distance = head - tail
-            if max(abs(distance)) > 1:
-                tail += np.sign(distance)
-            visited.add(tuple(tail))
+            for i in range(count):
+                for idx, knot in enumerate(knots):
+                    if idx == 0:
+                        knot += direction
+                    else:
+                        distance = knots[idx - 1] - knots[idx]
+                        if max(abs(distance)) > 1:
+                            knots[idx] += np.sign(distance)
+                visited.add(tuple(knots[-1]))
 
-    print(len(visited))
+        print(len(visited))
 
 if __name__ == '__main__':
     main()
