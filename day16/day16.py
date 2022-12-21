092172783b17851e9a069aae06f3135b3eb7e87b
Le Ax <axelsoot@gmail.com>
2022-12-20 18:30:58 -0800

    passes star 2 example but still duplicated solutions

--- day16/day16.py
+++ day16/day16.py
@@ -7,7 +7,9 @@ def main():
     file = open('input', 'r')
     lines = file.readlines()
     tunnels = Tunnels(lines)
-    best_solution = tunnels.prune_early()
+    best_solution = tunnels.prune_early(start_time=Tunnels.ALONE_STARTING_TIME, num_parallels=1)
+    print("Best solution {} tallies pressure {}".format(str(best_solution), int(best_solution)))
+    best_solution = tunnels.prune_early(start_time=Tunnels.ELEPHANT_STARTING_TIME, num_parallels=2)
     print("Best solution {} tallies pressure {}".format(str(best_solution), int(best_solution)))
 
 
@@ -45,34 +47,32 @@ class Tunnels:
             return self.input
 
     class Solution:
-        def __init__(self, other : "Tunnels"):
+        def __init__(self, other : "Tunnels", num_parallels=None, start_time=None):
             if isinstance(other, Tunnels):
-                self.valves = []
+                assert(num_parallels is not None and start_time is not None)
+                self.valves = [[]] * num_parallels
                 self.tunnels = other
-                self.remaining_time = Tunnels.STARTING_TIME
+                self.remaining_time = [start_time] * num_parallels
                 self.total_pressure = 0
             elif isinstance(other, Tunnels.Solution):
-                self.valves = other.valves.copy()
+                self.valves = [other.valves[i].copy() for i in range(len(other.valves))]
                 self.tunnels = other.tunnels
-                self.remaining_time = other.remaining_time
+                self.remaining_time = other.remaining_time.copy()
                 self.total_pressure = other.total_pressure
             else:
                 assert(False)
 
-        def add_valve(self, valve):
+        def add_valve(self, valve : "Tunnels.Valve", parralel_index):
             """ Return true iff there is enough time to go and open the added valve and thus the valve was added to the solution"""
-            if self.valves == []:
-                last_valve = self.tunnels.valves[Tunnels.START_VALVE]
-            else:
-                last_valve = self.valves[-1]
+            last_valve = self.tunnels.valves[Tunnels.START_VALVE] if self.valves[parralel_index] == [] else self.valves[parralel_index][-1]
 
             time_cost = last_valve.distances[valve.name] + 1
-            if time_cost > self.remaining_time:
+            if time_cost > self.remaining_time[parralel_index]:
                 return False
 
-            self.valves.append(valve)
-            self.remaining_time -= time_cost
-            self.total_pressure += self.remaining_time * valve.flow
+            self.valves[parralel_index].append(valve)
+            self.remaining_time[parralel_index] -= time_cost
+            self.total_pressure += self.remaining_time[parralel_index] * valve.flow
             return True
 
         def __iter__(self):
@@ -85,9 +85,12 @@ class Tunnels:
             return self.total_pressure
 
         def __str__(self):
-            return " -> ".join([valve.name for valve in self.valves])
+            if len(self.valves) == 1:
+                return " -> ".join([valve.name for valve in self.valves[0]])
+            return ", ".join([" -> ".join([valve.name for valve in self.valves[i]]) for i in range(len(self.valves))])
 
-    STARTING_TIME = 30
+    ALONE_STARTING_TIME = 30
+    ELEPHANT_STARTING_TIME = 26
     START_VALVE = "AA"
 
     def __init__(self, input):
@@ -121,25 +124,34 @@ class Tunnels:
                 useful_valves.append(valve)
         return useful_valves
 
-    def yield_solutions_for_remaining_valves(self, solution_so_far, remaining_valves):
+    def yield_solutions_for_remaining_valves(self, solution_so_far, remaining_valves, num_parallels):
         extended_solution = False
-        for idx, valve in enumerate(remaining_valves):
-            solution = Tunnels.Solution(solution_so_far)  # Clone solution
-            if solution.add_valve(valve):
-                extended_solution = True
-                remaining_valves_without_added = remaining_valves[0:idx] + remaining_valves[idx+1:]
-                yield from self.yield_solutions_for_remaining_valves(solution_so_far=solution, remaining_valves=remaining_valves_without_added)
+        for parralel_index in range(num_parallels):
+            for idx, valve in enumerate(remaining_valves):
+                solution = Tunnels.Solution(solution_so_far)  # Clone solution
+                if solution.add_valve(valve, parralel_index=parralel_index):
+                    extended_solution = True
+                    remaining_valves_without_added = remaining_valves[0:idx] + remaining_valves[idx+1:]
+                    yield from self.yield_solutions_for_remaining_valves(solution_so_far=solution,
+                                                                         remaining_valves=remaining_valves_without_added,
+                                                                         num_parallels=num_parallels)
 
         if not extended_solution:
             yield solution_so_far
 
-    def prune_early(self):
+    def prune_early(self, start_time, num_parallels):
         useful_valves = self.get_useful_valves()
         print("{} useful valves found".format(len(useful_valves)))
 
         best_solution = None
         idx = 0
-        for solution in self.yield_solutions_for_remaining_valves(Tunnels.Solution(self), useful_valves):
+        # More lines = safer job
+        initial_solution = Tunnels.Solution(self,
+                                            start_time=start_time,
+                                            num_parallels=num_parallels)
+        for solution in self.yield_solutions_for_remaining_valves(solution_so_far=initial_solution,
+                                                                  remaining_valves=useful_valves,
+                                                                  num_parallels=num_parallels):
             print("Found solution {}: {} {}".format(idx, int(solution), str(solution)))
             if best_solution is None or int(best_solution) < int(solution):
                 best_solution = solution
@@ -161,9 +173,12 @@ Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II
 """.splitlines()
     tunnels = Tunnels(example_input)
-    best_solution = tunnels.prune_early()
+    best_solution = tunnels.prune_early(start_time=Tunnels.ALONE_STARTING_TIME, num_parallels=1)
     assert(str(best_solution) == "DD -> BB -> JJ -> HH -> EE -> CC")
     assert(int(best_solution) == 1651)
+    best_solution = tunnels.prune_early(start_time=Tunnels.ELEPHANT_STARTING_TIME, num_parallels=2)
+    assert(str(best_solution) == "DD -> HH -> EE, JJ -> BB -> CC")
+    assert(int(best_solution) == 1707)
     print("unit tests passed")
 
 
