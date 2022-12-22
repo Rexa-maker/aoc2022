722358622a68b0abe31a920cd1afc2542fb7f2e9
Le Ax <axelsoot@gmail.com>
2022-12-21 18:54:50 -0800

    wip day17

--- day17/day17.py
+++ day17/day17.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 def main():
     unit_test()
 
@@ -6,13 +8,42 @@ def main():
     # TODO
 
 
+class Chamber:
+    WIDTH = 7
+
+    class Shape(Enum):
+        MINUS = 1
+        PLUS = 2
+        L = 3
+        I = 4
+        SQUARE = 5
+
+        @staticmethod
+        def next_shape(previous_shape=None):
+            if previous_shape in [None, Chamber.Shape.SQUARE]:
+                return Chamber.Shape.MINUS
+            elif previous_shape == Chamber.Shape.MINUS:
+                return Chamber.Shape.PLUS
+            elif previous_shape == Chamber.Shape.PLUS
+                return Chamber.Shape.L
+            elif previous_shape == Chamber.Shape.L:
+                return Chamber.Shape.I
+            elif previous_shape == Chamber.Shape.I:
+                return Chamber.Shape.SQUARE
+
+
+    def __init__(self):
+        self.falling_shape = None
+
+    def spawn_shape(self):
+        self.falling_shape = Chamber.Shape.next_shape(self.falling_shape)
+
+
 # TODO
 
 
 def unit_test():
-    example_input = """
-BLAH
-""".splitlines()
+    example_input = """>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>""".splitlines()
     # TODO
     print("unit tests passed")
 
