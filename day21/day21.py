4ebe4f5d8c644f4ab7d4f6cd2d3cb0703625bb5e
Le Ax <axelsoot@gmail.com>
2022-12-21 21:39:37 -0800

    day 21 star 2

--- day21/day21.py
+++ day21/day21.py
@@ -6,26 +6,51 @@ def main():
     troop = Troop(lines)
     root_number = troop.root_number()
     print("Root's yelled number is {}".format(root_number))
+    humn_goal = troop.humn_must_yell()
+    print("Humn must yell {}".format(humn_goal))
 
 
 class Troop:
     class Monkey:
-        def __init__(self, troop, name, number, others=None, operation=None):
+        def __init__(self, troop, name, number, others=None, operations=None):
             self.troop = troop
             self.name = name
             if number is not None:
                 self.number = number
-                self.others = self.operation = None
+                self.others = self.operations = None
             else:
                 self.others = others
-                self.operation = operation
+                self.operations = operations
                 self.number = None
 
         def yelled_number(self):
             if self.number is not None:
                 return self.number
             other_numbers = [self.troop.monkeys[self.others[i]].yelled_number() for i in range(2)]
-            return self.operation(other_numbers[0], other_numbers[1])
+            return self.operations[0](other_numbers[0], other_numbers[1])
+
+        def needs_humn(self):
+            """ Return true iff its output depends on humn"""
+            if self.name == "humn":
+                return True
+            if self.number is not None:
+                return False
+            return self.troop.monkeys[self.others[0]].needs_humn() or self.troop.monkeys[self.others[1]].needs_humn()
+
+        def humn_goal(self, goal):
+            if self.name == "humn":
+                return goal
+            if self.number is not None:
+                raise Exception("Cannot create goal from number!")
+            if self.troop.monkeys[self.others[0]].needs_humn():
+                right_number = self.troop.monkeys[self.others[1]].yelled_number()
+                left_goal = self.operations[2](right_number, goal)
+                return self.troop.monkeys[self.others[0]].humn_goal(left_goal)
+            else:
+                left_number = self.troop.monkeys[self.others[0]].yelled_number()
+                right_goal = self.operations[1](left_number, goal)
+                return self.troop.monkeys[self.others[1]].humn_goal(right_goal)
+
 
     def __init__(self, input):
         self.monkeys = {}
@@ -38,19 +63,42 @@ class Troop:
             except ValueError:
                 other1, operand, other2 = line[1].split(" ")
                 if operand == "+":
-                    operation = lambda x, y: x + y
+                    # g = x + y; x = g - y; y = g - x
+                    operation = lambda left, right: left + right
+                    reverse_operation_from_left = lambda left, goal: goal - left
+                    reverse_operation_from_right = lambda right, goal: goal - right
                 elif operand == "-":
-                    operation = lambda x, y: x - y
+                    # g = x - y; x = g + y; y = x - g
+                    operation = lambda left, right: left - right
+                    reverse_operation_from_left = lambda left, goal: left - goal
+                    reverse_operation_from_right = lambda right, goal: goal + right
                 elif operand == "/":
-                    operation = lambda x, y: x / y
+                    # g = x / y; x = g * y; y = x / g
+                    operation = lambda left, right: left / right
+                    reverse_operation_from_left = lambda left, goal: left / goal
+                    reverse_operation_from_right = lambda right, goal: goal * right
                 elif operand == "*":
-                    operation = lambda x, y: x * y
+                    # g = x * y; x = g / y; y = g / x
+                    operation = lambda left, right: left * right
+                    reverse_operation_from_left = lambda left, goal: goal / left
+                    reverse_operation_from_right = lambda right, goal: goal / right
                 else:
                     raise Exception("Unknown operand " + operand)
-                self.monkeys[name] = Troop.Monkey(troop=self, name=name, number=None, others=[other1, other2], operation=operation)
+                operations = [operation, reverse_operation_from_left, reverse_operation_from_right]
+                self.monkeys[name] = Troop.Monkey(troop=self, name=name, number=None, others=[other1, other2], operations=operations)
 
     def root_number(self):
         return self.monkeys["root"].yelled_number()
+
+    def humn_must_yell(self):
+        root_actors = self.monkeys["root"].others
+        if self.monkeys[root_actors[0]].needs_humn():
+            goal = self.monkeys[root_actors[1]].yelled_number()
+            humn_goal = self.monkeys[root_actors[0]].humn_goal(goal)
+        else:
+            goal = self.monkeys[root_actors[0]].yelled_number()
+            humn_goal = self.monkeys[root_actors[1]].humn_goal(goal)
+        return humn_goal
 
 
 def unit_test():
@@ -73,6 +121,9 @@ hmdt: 32
     troop = Troop(example_input)
     root_number = troop.root_number()
     assert(root_number == 152)
+    humn_goal = troop.humn_must_yell()
+    print(humn_goal)
+    assert(humn_goal == 301)
     print("unit tests passed")
 
 
