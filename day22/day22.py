940555dee785100e627856f3142a4006cea41dcf
Le Ax <axelsoot@gmail.com>
2022-12-21 21:41:05 -0800

    day 22 input

---
+++ day22/day22.py
@@ -0,0 +1,21 @@
+def main():
+    unit_test()
+
+    file = open('input', 'r')
+    lines = file.readlines()
+    # TODO
+
+
+# TODO
+
+
+def unit_test():
+    example_input = """
+BLAH
+""".splitlines()
+    # TODO
+    print("unit tests passed")
+
+
+if __name__ == '__main__':
+    main()
